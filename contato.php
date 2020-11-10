c6d0c3c4d0d759c1e006b8013c67d008ed4e287a
Taina Ferreira Rodrigues <tferreirarodrigues1@gmail.com>
2020-11-09 21:15:47 -0300

    Inclusão de bootstrap

--- contato.php
+++ contato.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contato - Fale conosco</title>
     <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
     <!--Menu-->
@@ -29,20 +30,34 @@
     <!--Fim de Imagens e dados do setor de contato-->
 
     <!--Inicio do formulário-->
+    
     <form>
-        <h4>Nome:</h4>
-        <input type="text" style="width: 400px;">
-        <h4>Mensagem:</h4>
-        <textarea style="width: 400px;"></textarea>
+    <div class form-group>
+        <label for="Nome">Digite seu nome</label>
+        <input type="text" class="form-control" id="nome" aria-describedby="nome" placeholder="Ex: James">
+    </div>
 
-        <button type="submit">Enviar</button>
+    <div class form-group>
+        <label for="email">Digite seu email</label>
+        <input type="email" class="form-control" id="email" aria-describedby="email" placeholder="eusoujames@provedor.com">
+    </div>   
+
+    <div class form-group>
+        <label for="Mensagem">Digite aqui sua mensagem.</br> Entraremos em contato o mais breve possível.</label>
+        <input type="text" class="form-control" id="mensagem" aria-describedby="mensagem">
+    </div> 
+        <button type="submit"class="btn btn-primary">Enviar</button>
     </form>
+    
     <!--Fim do formulário-->
 
     <!--Alinhamento--> 
     <br> <br> <br> <br> <br>
     <br> <br> <br> <br> <br>
 
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
    
     <footer id="rodape">
         <p id="Formas_de_Pagamento"><b>Formas de Pagamento:</b></p>
--- produtos.php
+++ produtos.php
@@ -10,6 +10,7 @@
     <title>Produtos - Full Stack Eletro</title>
     <link rel="stylesheet" type="text/css" href="css/estilo.css">
     <script src="js/funcoes.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -17,51 +18,57 @@
     <?php 
         include_once('menu.html');
     ?>
+    <div class="container-fluid">
+        <div class="row">
+            <header>
+                <h2>Produtos</h2>
+            </header>
+        </div>
+    </div>
+    <hr class="my-4">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-2">
+                <section class="categorias">
+                    <h3>Categorias</h3>
+                    <ul>
+                        <li onclick="exibirTodos()">Todos</li>
+                        <li onclick="exibirCategorias('Geladeira')">Geladeira</li>
+                        <li onclick="exibirCategorias('Fogão')">Fogão</li>
+                        <li onclick="exibirCategorias('Micro-ondas')">Micro-ondas</li>
+                        <li onclick="exibirCategorias('Lavadora de roupas')">Lavadora de Roupas</li>
+                        <li onclick="exibirCategorias('Lava Louças')">Lava-louças</li>
+                    </ul>
+                </section>
+            </div>
 
-    <header>
-        <h2>Produtos</h2>
-    </header>
-    <hr>
+            <div class="col-10">
+                <section class="produtos">
+                    <?php
+                        $produtos = buscarProdutos();
+                        if($produtos->num_rows){
+                            while ($row = $produtos->fetch_assoc()) {
+                                ?>
+                                    <div class="card" id="<?php echo $row['categoria']; ?>">
+                                        <img src="<?php echo $row['imagem']; ?>"onmouseover="destaque(this)" onmouseout="destaque(this)">
+                                        <p><?php echo $row['descricao']; ?></p>
+                                        <hr>
+                                        <p>R$ <?php echo $row['preco']; ?></p>
+                                        <p>R$ <?php echo $row['preco_com_desconto']; ?></p>
+                                    </div>
+                                <?php
+                            }
+                        }
+                    ?>
+                </section>
+            </div>
+        </div>
+    </div>
 
-    <main class="main_produtos">
-        <section class="categorias">
-            <h3>Categorias</h3>
-            <ul>
-                <li onclick="exibirTodos()">Todos</li>
-                <li onclick="exibirCategorias('Geladeira')">Geladeira</li>
-                <li onclick="exibirCategorias('Fogão')">Fogão</li>
-                <li onclick="exibirCategorias('Micro-ondas')">Micro-ondas</li>
-                <li onclick="exibirCategorias('Lavadora de roupas')">Lavadora de Roupas</li>
-                <li onclick="exibirCategorias('Lava Louças')">Lava-louças</li>
-            </ul>
-        </section>
 
-
-        <section class="produtos">
-
-            <?php
-                $produtos = buscarProdutos();
-                if($produtos->num_rows){
-                    while ($row = $produtos->fetch_assoc()) {
-                        ?>
-                            <div class="card" id="<?php echo $row['categoria']; ?>">
-                                <img src="<?php echo $row['imagem']; ?>"onmouseover="destaque(this)" onmouseout="destaque(this)">
-                                <p><?php echo $row['descricao']; ?></p>
-                                <hr>
-                                <p>R$ <?php echo $row['preco']; ?></p>
-                                <p>R$ <?php echo $row['preco_com_desconto']; ?></p>
-                            </div>
-                        <?php
-                    }
-                }
-            ?>
-
-        </section>
-    </main>
-
-    <br> <br> <br> <br> <br>
-    <br> <br> <br> <br> <br>
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>            
 
     <footer id="rodape">
         <p id="Formas_de_Pagamento"><b>Formas de Pagamento:</b></p>
